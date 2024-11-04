f3ff22be7b121d1eb38d89e1a5f3f914e38dc174
cjdelacruz <cjdelacruz0521@gmail.com>
2024-11-04 19:22:16 +0700

    Initial commit with userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+// Create a user profile object
+let userProfile = {
+    name: "Cassandra Jade H. Dela Cruz",
+    age: 19,
+    favoriteNumber: 13,
+    favoriteColors: ["black", "green", "white"]
+  };
+  
+  // Print the user profile object
+  console.log(userProfile);
